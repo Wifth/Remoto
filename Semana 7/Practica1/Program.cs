107b2d6787702a75878dd7175753af1f38ef81b8
Wifth <alexander.obando.os2206@gmail.com>
2024-09-26 09:12:29 -0500

    Problema 13.1

--- Semana 7/Practica1/Program.cs
+++ Semana 7/Practica1/Program.cs
@@ -1,3 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-//Semana 7
+//Ejercicio 13
+
+int T = 0, s = 0, f = 0, sm, dm;
+
+Console.Write("Ingrese la cantidad de trabajadores: ");
+T = int.Parse(Console.ReadLine());
+
+for (int x = 1; x <= T; x++) 
+{
+    Console.Write("Ingrese su nombre: ");
+    string n = Console.ReadLine();
+    Console.Write("Ingrese su sueldo: ");
+    s = int.Parse(Console.ReadLine());
+    Console.Write("Ingrese la cantidad de faltas: ");
+    f = int.Parse(Console.ReadLine());
+
+    sm = s 
+    Console.Write("Días trabajados: " + dm);
+    Console.Write("Sueldo: " + sm);
+}
+
